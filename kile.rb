a9c88755d01b3aaab3a258451bd47a42da3ef7a5
laflij <laflijj@gmail.com>
2014-02-23 22:44:45 -0500

    added kile

---
+++ kile.rb
@@ -0,0 +1,11 @@
+require File.join(File.dirname(__FILE__), 'base_kde_formula')
+
+class Kile < BaseKdeFormula
+  homepage 'http://www.kde.org/applications/office/kile/'
+  url 'http://kile.sourceforge.net/download.php?get=http://sourceforge.net/projects/kile/files/stable/kile-2.1.3/kile-2.1.3.tar.bz2/download'
+  sha1 ''
+
+  depends_on 'kdelibs'
+  depends_on 'kde-runtime'
+  kde_build_deps
+end
