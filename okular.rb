f7541e3103fe0ce3fbe49cfd3ee53f6edcc3c3da
laflij <laflijj@gmail.com>
2014-02-25 22:50:34 -0500

    okular url update

--- okular.rb
+++ okular.rb
@@ -2,8 +2,8 @@ require File.join(File.dirname(__FILE__), 'base_kde_formula')
 
 class Okular < BaseKdeFormula
   homepage 'http://okular.kde.org/'
-  url '/users/abc/src/git.git', :using => :git
-  sha1 ' '
+  url 'http://anongit.kde.org/okular/okular-latest.tar.gz', :using => :git
+  sha1 ''
 
   depends_on 'kdelibs'
   depends_on 'kde-runtime'
