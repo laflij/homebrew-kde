36ae01ec03c0e23fd569ddd86f27577c75ee6e64
laflij <laflijj@gmail.com>
2014-02-25 22:53:57 -0500

    added sh1 key

--- okular.rb
+++ okular.rb
@@ -3,7 +3,7 @@ require File.join(File.dirname(__FILE__), 'base_kde_formula')
 class Okular < BaseKdeFormula
   homepage 'http://okular.kde.org/'
   url 'http://anongit.kde.org/okular/okular-latest.tar.gz'
-  sha1 ''
+  sha1 'e01f2aae8dfac50006eddab805d80a1256fb27a3'
 
   depends_on 'kdelibs'
   depends_on 'kde-runtime'
