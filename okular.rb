f4d0f3f00c2d40053a71c88a6b04fc756391bfb0
laflij <laflijj@gmail.com>
2014-02-26 09:56:29 -0500

    updated url, modified install

--- okular.rb
+++ okular.rb
@@ -2,10 +2,15 @@ require File.join(File.dirname(__FILE__), 'base_kde_formula')
 
 class Okular < BaseKdeFormula
   homepage 'http://okular.kde.org/'
-  url '/anongit.kde.org/okular', :using => :git  
+  url 'http://anongit.kde.org/okular/okular-latest.tar.gz '
   sha1 'e01f2aae8dfac50006eddab805d80a1256fb27a3'
 
   depends_on 'kdelibs'
   depends_on 'kde-runtime'
   kde_build_deps
+
+    def install
+	system "./initrepo.sh"
+	default_install	
+    end
 end
